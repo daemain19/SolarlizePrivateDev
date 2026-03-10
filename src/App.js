341da2670dc08d37cbb57756c11f6f4f3cc50f63
daemain19 <hptrivedi1@sheffield.ac.uk>
2026-03-10 14:26:31 +0000

    UI and globe along with textures

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import StaffDashboard from './pages/StaffDashboard';
-
 function App() {
   return (
     <Router>
